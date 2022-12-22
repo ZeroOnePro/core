d92a8435b2eee0e86b399bf7071d26f3a094bc07
ZeroOnePro <dbtjdals1771@ajou.ac.kr>
2022-12-22 13:10:01 +0900

    feat: add request scope bean logger

---
+++ src/main/java/hello/core/web/LogDemoController.java
@@ -0,0 +1,30 @@
+package hello.core.web;
+
+import hello.core.common.MyLogger;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+@RequiredArgsConstructor
+public class LogDemoController {
+
+    private final LogDemoService logDemoService;
+    private final MyLogger myLogger;
+
+    @RequestMapping("log-demo")
+    @ResponseBody // view 없이 단순 문자열 반
+    // HttpServletRequest 는 자바에서 제공하는 표준 서블릿 규약에 따라 HTTP 요청을 받게한다
+    public String logDemo(HttpServletRequest request){
+        String requestURL = request.getRequestURL().toString();
+        myLogger.setRequestURL(requestURL);
+
+        myLogger.log("controller test");
+        logDemoService.logic("testId");
+
+        return "OK";
+    }
+}
